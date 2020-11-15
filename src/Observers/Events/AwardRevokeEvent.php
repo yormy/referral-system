18cf4c88968a54356ba81d6b1db0502ca2ece52b
yormy <yormy@users.noreply.github.com>
2020-11-15 18:23:11 +0000

    Fix styling

--- src/Observers/Events/AwardRevokeEvent.php
+++ src/Observers/Events/AwardRevokeEvent.php
@@ -7,13 +7,14 @@ use Illuminate\Queue\SerializesModels;
 
 class AwardRevokeEvent
 {
-    use Dispatchable, SerializesModels;
+    use Dispatchable;
+    use SerializesModels;
 
     public string $actionId;
 
     public string $deleteReason;
 
-    public function __construct(string $actionId, string $deleteReason ="")
+    public function __construct(string $actionId, string $deleteReason = "")
     {
         $this->actionId = $actionId;
         $this->deleteReason = $deleteReason;
--- src/Observers/Listeners/AwardRevokeListener.php
+++ src/Observers/Listeners/AwardRevokeListener.php
@@ -26,8 +26,6 @@ class AwardRevokeListener
                 $latestReward->save();
                 $latestReward->delete();
             }
-
         }
-
     }
 }
