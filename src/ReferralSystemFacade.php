0935ad0c5f33e675bc3cc1e0f87a2000a21a56d0
rob <dev@mexion.com>
2021-02-19 15:07:47 +0100

    fix facade

--- src/ReferralSystemFacade.php
+++ src/ReferralSystemFacade.php
@@ -4,13 +4,10 @@ namespace Yormy\ReferralSystem;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @see \Yormy\ReferralSystem\ReferralSystem
- */
 class ReferralSystemFacade extends Facade
 {
-//    protected static function getFacadeAccessor()
-//    {
-//        return 'referral-system';
-//    }
+    protected static function getFacadeAccessor()
+    {
+        return 'ReferralSystem';
+    }
 }
